d40e657d75aaff08d05505413f853ec693b2c1a4
saad <msaadsaif5@gmail.com>
2018-10-11 17:57:25 +0300

    initial commit

---
+++ Chatty/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Chatty.Infrastructure;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatty.Hubs
+{
+    public class ChatHub : Hub<IChattyClient>
+    {
+        public Task BroadcastMessage(string message)
+        {
+            return Clients.All.ReceiveMessage(GetUserName(), message);
+        }
+
+        public void SendMessageToOthers(string message)
+        {
+            Clients.Others.ReceiveMessage(GetUserName(), message);
+        }
+
+        public void SendMessageToCaller(string message)
+        {
+            Clients.Caller.ReceiveMessage(GetUserName(), message);
+        }
+
+        private string GetUserName()
+        {
+            return Context.User?.Identity?.Name ?? "anonymous";
+        }
+    }
+}
---
+++ Chatty/Infrastructure/IChattyClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatty.Infrastructure
+{
+    public interface IChattyClient
+    {
+        Task ReceiveMessage(string user, string message);
+    }
+}
